e6e6649edb665c18a273f39cde92ec5a47f086c0
tranhuyland <huy07kt@gmail.com>
2026-05-29 13:57:13 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import PropertyGrid from '@/components/PropertyGrid';
+import Footer from '@/components/Footer';
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <PropertyGrid />
+      <Footer />
+    </>
+  );
+}
